d236c44670803d91ba92d6279358481cc7abd90b
Yurii Omelchuk <omelchukyuriy1998@gmail.com>
2022-05-15 23:59:05 +0300

    day 2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-const getResource = async (url) => {
-   const res = await fetch(url);
-   if (!res.ok) {
-      throw new Error(`Could not fetch ${url}` + `, received ${res.status}`)
-   }
-   const body = await res.json();
-   return body;
-}
-getResource('https://swapi.dev/api/people/156512/')
-   .then((body) => {
-      console.log(body);
-   })
-   .catch((err) => {
-      console.error('Could not fetch', err);
-   });
+ReactDOM.render(<App />,
+   document.getElementById('root'));
