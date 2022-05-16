99911163a0ff14d67e4911d0934b2bad41b5a75f
Yurii Omelchuk <omelchukyuriy1998@gmail.com>
2022-05-17 01:23:48 +0300

    day 3.5

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -57,6 +57,7 @@ export default class App extends Component {
                   <ItemList
                      onItemSelected={this.onPersonSelected}
                      getData={this.swapiService.getAllPlanet}
+                     renderItem={(item) => item.name}
                   />
                </div>
                <div className="col-md-6">
@@ -68,6 +69,7 @@ export default class App extends Component {
                   <ItemList
                      onItemSelected={this.onPersonSelected}
                      getData={this.swapiService.getAllStarships}
+                     renderItem={(item) => item.name}
                   />
                </div>
                <div className="col-md-6">
